6cfebf4954d03f190e2d76881f2382bad5997712
Filip Drobnjakovic <drobnjakovicfilip@gmail.com>
2023-05-28 14:44:24 +0200

    Move in config overrides, fix test resources.
    Signed-off-by: Filip Drobnjakovic <drobnjakovicfilip@gmail.com>

---
+++ src/main/java/org/opensearch/performanceanalyzer/commons/stats/decisionmaker/DecisionMakerConsts.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.performanceanalyzer.commons.stats.decisionmaker;
+
+
+import com.google.gson.JsonParser;
+
+public class DecisionMakerConsts {
+    public static final String CACHE_MAX_WEIGHT = "maximumWeight";
+
+    public static final JsonParser JSON_PARSER = new JsonParser();
+}
