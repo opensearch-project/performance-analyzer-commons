872ea5f18504f5d84b7bf2e015b194ffd1664c06
Kartavya Vashishtha <sendtokartavya@gmail.com>
2023-10-17 14:44:12 -0400

    wip commit
    Signed-off-by: Kartavya Vashishtha <sendtokartavya@gmail.com>

--- src/test/java/org/opensearch/performanceanalyzer/commons/os/ThreadDiskIOTests.java
+++ src/test/java/org/opensearch/performanceanalyzer/commons/os/ThreadDiskIOTests.java
@@ -9,16 +9,16 @@ package org.opensearch.performanceanalyzer.commons.os;
 import org.junit.Test;
 
 public class ThreadDiskIOTests {
-    public static void main(String[] args) throws Exception {
-        runOnce();
-    }
 
     public static void runOnce() {
         ThreadDiskIO.addSample();
         System.out.println(ThreadDiskIO.getIOUtilization().toString());
     }
 
-    // - to enhance
     @Test
-    public void testMetrics() {}
+    public void testMetrics() {
+        // this test checks that
+        // 1. ThreadDiskIO calls the SchemaFileParser constructor with the correct path
+        // 2. ThreadDiskIO calculates the correct metrics from procfile data
+    }
 }
